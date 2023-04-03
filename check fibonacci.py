121e71bee5b156ab24f2993dbc091c18cb29ac6b
jorgianepires1 <57422830+jorgianepires1@users.noreply.github.com>
2023-04-03 09:36:05 -0300

    Create check fibonacci.py

---
+++ check fibonacci.py
@@ -0,0 +1,11 @@
+def check_fibonacci(numero):
+    a, b = 0, 1
+    while b < numero:
+        a, b = b, a+b
+    if b == numero:
+        print(f"{numero} pertence à sequência de Fibonacci.")
+    else:
+        print(f"{numero} não pertence à sequência de Fibonacci.")
+
+
+check_fibonacci(2)
